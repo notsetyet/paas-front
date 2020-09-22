67da9de1224d5170c9f047b2ecf1ebb7e8781ded
zyj1721 <957379307@qq.com>
2020-09-22 17:04:40 +0800

    管理容器

--- code/src/App.vue
+++ code/src/App.vue
@@ -1,130 +1,140 @@
 <style scoped>
-  #app {
+#app {
     font-family: Avenir, Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
     color: #2c3e50;
-  }
+}
 
-  .layout{
+.layout {
     border: 1px solid #d7dde4;
     background: #f5f7f9;
     position: relative;
     border-radius: 4px;
     overflow: hidden;
-  }
-  .layout-breadcrumb{
+}
+
+.layout-breadcrumb {
     padding: 10px 15px 0;
-  }
-  .layout-content{
+}
+
+.layout-content {
     min-height: 200px;
     margin: 15px;
     overflow: hidden;
     background: #fff;
     border-radius: 4px;
-  }
-  .layout-content-main{
+}
+
+.layout-content-main {
     padding: 10px;
-  }
-  .layout-copy{
+}
+
+.layout-copy {
     text-align: center;
     padding: 10px 0 20px;
     color: #9ea7b4;
-  }
-  .layout-menu-left{
+}
+
+.layout-menu-left {
     background: #464c5b;
-  }
-  .layout-header{
+}
+
+.layout-header {
     height: 60px;
     background: #fff;
-    box-shadow: 0 1px 1px rgba(0,0,0,.1);
-  }
-  .layout-logo-left{
+    box-shadow: 0 1px 1px rgba(0, 0, 0, .1);
+}
+
+.layout-logo-left {
     width: 90%;
     height: 30px;
     background: #5b6270;
     border-radius: 3px;
     margin: 15px auto;
-  }
-  .layout-ceiling-main a{
+}
+
+.layout-ceiling-main a {
     color: #9ba7b5;
-  }
-  .layout-hide-text .layout-text{
+}
+
+.layout-hide-text .layout-text {
     display: none;
-  }
-  .ivu-col{
+}
+
+.ivu-col {
     transition: width .2s ease-in-out;
-  }
+}
 </style>
 <template>
-  <div id="app">
+<div id="app">
     <div class="layout" :class="{'layout-hide-text': spanLeft < 5}">
-      <Row type="flex">
-        <i-col :span="spanLeft" class="layout-menu-left">
-          <Menu active-key="1" theme="dark" width="auto">
-            <div class="layout-logo-left"></div>
-            <Submenu key="1" to="service-list">
-              <template slot="title">
-                <Icon type="ios-navigate" :size="iconSize"></Icon>
-                服务页
-              </template>
-              <Menu-item key="1-1" to="service-list">服务列表</Menu-item>
-              <Menu-item key="1-1">创建容器</Menu-item>
-            </Submenu>
-            <Submenu key="2" to="service-list">
-              <template slot="title">
-                <Icon type="ios-navigate" :size="iconSize"></Icon>
-                管理页
-              </template>
-              <Menu-item key="2-1" to="service-list">服务列表</Menu-item>
-              <Menu-item key="2-2" to="create-container">创建容器</Menu-item>
-            </Submenu>
-          </Menu>
-        </i-col>
-        <i-col :span="spanRight">
-          <div class="layout-header">
-            <i-button type="text" @click="toggleClick" style="float: left">
-              <Icon type="ios-people" size="32"></Icon>
-            </i-button>
-<!--            无法显示-->
-            <Button to="/login" v-if="uid==null">登录/注册</Button>
-            <Button to="/login" v-if="uid!=null">{{this.username}}</Button>
-          </div>
-          <router-view/>
-          <div class="layout-copy">
-            2020-2020 &copy; PAAS
-          </div>
-        </i-col>
-      </Row>
+        <Row type="flex">
+            <i-col :span="spanLeft" class="layout-menu-left">
+                <Menu active-key="1" theme="dark" width="auto">
+                    <div class="layout-logo-left"></div>
+                    <Submenu key="1">
+                        <template slot="title">
+                            <Icon type="ios-navigate" :size="iconSize"></Icon>
+                            服务页
+                        </template>
+                        <Menu-item key="1-1" to="service-list">服务列表</Menu-item>
+                        <Menu-item key="1-1" to="create-container">创建容器</Menu-item>
+                    </Submenu>
+                    <Submenu key="2">
+                        <template slot="title">
+                            <Icon type="ios-navigate" :size="iconSize"></Icon>
+                            管理页
+                        </template>
+                        <Menu-item key="2-1" to="manage-container">管理容器</Menu-item>
+                    </Submenu>
+                </Menu>
+            </i-col>
+            <i-col :span="spanRight">
+                <div class="layout-header">
+                    <i-button type="text" @click="toggleClick" style="float: left">
+                        <Icon type="ios-people" size="32"></Icon>
+                    </i-button>
+                    <!--            无法显示-->
+                    <Button to="/login" v-if="uid==null">登录/注册</Button>
+                    <Button to="/login" v-if="uid!=null">{{this.username}}</Button>
+                </div>
+                <router-view />
+                <div class="layout-copy">
+                    2020-2020 &copy; PAAS
+                </div>
+            </i-col>
+        </Row>
     </div>
-  </div>
+</div>
 </template>
+
 <script>
-  export default {
-    data () {
-      return {
-        spanLeft: 5,
-        spanRight: 19,
-        username:"",
-        uid:""
-      }
+export default {
+    data() {
+        return {
+            spanLeft: 5,
+            spanRight: 19,
+            username: "",
+            uid: ""
+        }
     },
     computed: {
-      iconSize () {
-        return this.spanLeft === 5 ? 14 : 24;
-      }
+        iconSize() {
+            return this.spanLeft === 5 ? 14 : 24;
+        }
     },
     methods: {
-      toggleClick () {
-        if (this.spanLeft === 5) {
-          this.spanLeft = 2;
-          this.spanRight = 22;
-        } else {
-          this.spanLeft = 5;
-          this.spanRight = 19;
+        toggleClick() {
+            if (this.spanLeft === 5) {
+                this.spanLeft = 2;
+                this.spanRight = 22;
+            } else {
+                this.spanLeft = 5;
+                this.spanRight = 19;
+            }
         }
-      }
     }
-  }
+}
 </script>
---
+++ code/src/views/ManageContainer.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="manage-container">
+        <div class="layout-breadcrumb">
+            <Breadcrumb>
+                <BreadcrumbItem to="/">首页</BreadcrumbItem>
+                <BreadcrumbItem to="/">管理页</BreadcrumbItem>
+                <BreadcrumbItem>管理容器</BreadcrumbItem>
+            </Breadcrumb>
+            <h2 style="font-weight: bold">管理容器</h2>
+        </div>
+        <div class="layout-content">
+            <Card>
+
+            <CellGroup style="text-align: left;">
+            <Cell v-for="(con, index) in conList" :key="index" :title="con.name 
+            +' | '
+            + con.mirror
+            +' | '
+            + con.owner
+            +' | '
+            + con.port">
+                <Button type="primary" slot="extra" style="margin-left: 10px;" >进入容器</Button>
+                <Button type="error" slot="extra" style="margin-left: 10px;">删除</Button>
+            </Cell>
+            </CellGroup>
+
+            </Card>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "ManageContainer",
+        data () {
+            return {
+                conList: [
+                    {
+                        name: 'This i',
+                        mirror: 'This is ',
+                        owner: '9avatar',
+                        port: 'This is the this is.'
+                    },
+                ]
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
